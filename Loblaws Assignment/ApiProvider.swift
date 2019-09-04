c9c01383b50b37803241784a5b7806b4e20e0956
Saim Zahid <saimzahid@me.com>
2019-09-04 17:05:44 -0400

    Initial Commit.

---
+++ Loblaws Assignment/ApiProvider.swift
@@ -0,0 +1,61 @@
+//
+//  ApiProvider.swift
+//  Loblaws Assignment
+//
+//  Created by Saim Zahid on 2019-09-04.
+//  Copyright © 2019 Saim Zahid. All rights reserved.
+//
+
+import UIKit
+
+class ApiProvider {
+    public static let shared = ApiProvider()
+    
+    public enum APIServiceError: Error {
+        case apiError
+        case invalidEndpoint
+        case invalidResponse
+        case noData
+        case decodeError
+    }
+    
+    private let urlSession = URLSession.shared
+    
+    private let jsonDecoder: JSONDecoder = {
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        return jsonDecoder
+    }()
+    
+    
+    private func fetchResources<T: Decodable>(url: URL, completion: @escaping (Result<T, APIServiceError>) -> Void) {
+        guard let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
+            completion(.failure(.invalidEndpoint))
+            return
+        }
+        
+        guard let url = urlComponents.url else {
+            completion(.failure(.invalidEndpoint))
+            return
+        }
+        
+        urlSession.dataTask(with: url) { (result) in
+            switch result {
+            case .success(let (response, data)):
+                guard let statusCode = (response as? HTTPURLResponse)?.statusCode, 200..<299 ~= statusCode else {
+                    completion(.failure(.invalidResponse))
+                    return
+                }
+                do {
+                    let values = try self.jsonDecoder.decode(T.self, from: data)
+                    completion(.success(values))
+                } catch {
+                    completion(.failure(.decodeError))
+                }
+            case .failure(_):
+                completion(.failure(.apiError))
+            }
+            }.resume()
+    }
+}
+
---
+++ Loblaws Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Loblaws Assignment
+//
+//  Created by Saim Zahid on 2019-09-04.
+//  Copyright © 2019 Saim Zahid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
